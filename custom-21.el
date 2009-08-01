adedb524d5790a3805f171e42d57043d25f097e0
Jimi Xenidis <jimix@watson.ibm.com>
2009-08-01 07:27:12 -0500

    allow use for emacs21

---
+++ custom-21.el
@@ -0,0 +1,47 @@
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(asm-comment-char 35)
+ '(blink-cursor-mode nil)
+ '(comint-password-prompt-regexp "\\(\\(GSA \\|Enter \\|[Oo]ld \\|[Nn]ew \\|'s \\|login \\|Kerberos \\|CVS \\|UNIX \\| SMB \\|LDAP \\|\\[sudo] \\|^\\)[Pp]assword\\( (again)\\)?\\|pass phrase\\|\\(Enter \\|Repeat \\|Bad \\)?[Pp]assphrase\\)\\(?:, try again\\)?\\(?: for [^:]+\\)?:\\s *\\'")
+ '(compilation-ask-about-save nil)
+ '(compilation-scroll-output t)
+ '(compilation-skip-threshold 0)
+ '(compilation-skip-visited t)
+ '(compile-command "make ")
+ '(cscope-projects-file "~/.emacs.d/cscope-projects.el")
+ '(cvs-auto-remove-handled t)
+ '(cvs-find-file-and-jump nil)
+ '(focus-follows-mouse t)
+ '(grep-find-command "find . \\( -name CVS -o -name SCCS -o -name .hg -o -name .git \\) -prune -o \\! \\( -name \\*~ -o -name .\\#\\* -o -name cscope.\\* \\) -type f -print0 | xargs -0 grep -n -e")
+ '(ido-default-file-method (quote selected-window))
+ '(ido-everywhere t)
+ '(indicate-buffer-boundaries nil)
+ '(log-edit-confirm t)
+ '(menu-bar-mode t)
+ '(mouse-autoselect-window t)
+ '(mouse-wheel-progressive-speed nil)
+ '(save-place t nil (saveplace))
+ '(show-paren-mode t)
+ '(show-trailing-whitespace t)
+ '(ssh-directory-tracking-mode t)
+ '(tool-bar-mode nil)
+ '(tramp-debug-buffer t)
+ '(tramp-terminal-type "tramp")
+ '(truncate-partial-width-windows nil)
+ '(uniquify-buffer-name-style (quote post-forward-angle-brackets) nil (uniquify))
+ '(vc-diff-switches "-u")
+ '(visible-bell t)
+ '(warning-suppress-types (quote ((undo discard-info))))
+ '(windmove-wrap-around t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:stipple nil :background "black" :foreground "yellow" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 18 :width condensed :family "schumacher-clean"))))
+ '(cursor ((t (:background "red"))))
+)
